f02c5e1ccd5254c35b09663aa99ca1d82698dbe0
Jeferson Vinícius <jeferson.viniciuscrc@gmail.com>
2021-04-13 21:21:19 -0300

    Add relation resolvers

--- GraphQL/graph/schema.resolvers.go
+++ GraphQL/graph/schema.resolvers.go
@@ -12,6 +12,28 @@ import (
 	"github.com/jefersonvinicius/microservices-course-communication/GraphQL/graph/model"
 )
 
+func (r *categoryResolver) Courses(ctx context.Context, obj *model.Category) ([]*model.Course, error) {
+	var courses []*model.Course
+	for _, v := range r.Resolver.Courses {
+		if v.Category.ID == obj.ID {
+			courses = append(courses, v)
+		}
+	}
+
+	return courses, nil
+}
+
+func (r *courseResolver) Chapters(ctx context.Context, obj *model.Course) ([]*model.Chapter, error) {
+	var chapters []*model.Chapter
+	for _, v := range r.Resolver.Chapters {
+		if v.Course.ID == obj.ID {
+			chapters = append(chapters, v)
+		}
+	}
+
+	return chapters, nil
+}
+
 func (r *mutationResolver) CreateCategory(ctx context.Context, input model.NewCategory) (*model.Category, error) {
 	category := model.Category{
 		ID:          fmt.Sprintf("T%d", rand.Int()),
@@ -41,11 +63,9 @@ func (r *mutationResolver) CreateCourse(ctx context.Context, input model.NewCour
 	r.Resolver.Courses = append(r.Resolver.Courses, &course)
 
 	return &course, nil
-
 }
 
 func (r *mutationResolver) CreateChapter(ctx context.Context, input model.NewChapter) (*model.Chapter, error) {
-
 	var course *model.Course
 	for _, c := range r.Resolver.Courses {
 		if c.ID == input.CourseID {
@@ -63,7 +83,6 @@ func (r *mutationResolver) CreateChapter(ctx context.Context, input model.NewCha
 	r.Resolver.Chapters = append(r.Resolver.Chapters, &chapter)
 
 	return &chapter, nil
-
 }
 
 func (r *queryResolver) Categories(ctx context.Context) ([]*model.Category, error) {
@@ -78,11 +97,19 @@ func (r *queryResolver) Chapters(ctx context.Context) ([]*model.Chapter, error) 
 	return r.Resolver.Chapters, nil
 }
 
+// Category returns generated.CategoryResolver implementation.
+func (r *Resolver) Category() generated.CategoryResolver { return &categoryResolver{r} }
+
+// Course returns generated.CourseResolver implementation.
+func (r *Resolver) Course() generated.CourseResolver { return &courseResolver{r} }
+
 // Mutation returns generated.MutationResolver implementation.
 func (r *Resolver) Mutation() generated.MutationResolver { return &mutationResolver{r} }
 
 // Query returns generated.QueryResolver implementation.
 func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
 
+type categoryResolver struct{ *Resolver }
+type courseResolver struct{ *Resolver }
 type mutationResolver struct{ *Resolver }
 type queryResolver struct{ *Resolver }
